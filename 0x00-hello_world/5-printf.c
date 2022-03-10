acdf61ec36f1d1dc63f88360c96d2746df6f40ae
KEVINVOKE <kevinopon730@gmail.com>
2022-03-10 13:08:33 -0800

    My commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 /**
  * main - entry point
  * @void: no arguments
- * Description: the program starts and ends here.
- * Return: value 0 to exit
+ * Descripion: the program starts and ends here.
+ * Return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n"
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
