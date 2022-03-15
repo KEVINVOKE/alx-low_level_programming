fd53869cc9054e4e89aa77509ebbf4cee8135359
KEVINVOKE <kevinopon730@gmail.com>
2022-03-15 13:01:54 -0700

    My commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
 *_islower - check for lower case letters
+*@c: input.
 *Return: 1 or 0
 */
 int _islower(int c)
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
 *_isalpha - check for uppercase letters
+*@c: input.
 *Return: 1 if c is uppercase 0 if c is anyhting else.
 */
 int _isalpha(int c)
