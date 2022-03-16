8d1af92a6d6574a050adaff93c48cae0650e4b96
KEVINVOKE <kevinopon730@gmail.com>
2022-03-15 22:58:37 -0700

    My commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,41 +6,42 @@
 */
 void print_times_table(int n)
 {
-int a = 0, rep, b;
-if (n < 0 || n > 15)
-return;
-while (a <= n)
+int i, j, k;
+if (n >= 0 && n <= 15)
 {
-for (b = 0; b <= n; b++)
+for (i = 0; i <= n; i++)
 {
-rep = a * b;
-if (b == 0)
-_putchar('0' + rep);
-else if (rep < 10)
+_putchar('0');
+if (n != 0)
+_putchar(',');
+for (j = 1; j <= n; j++)
+{
+k = i * j;
+if (k < 10)
 {
 _putchar(' ');
 _putchar(' ');
-_putchar('0' + rep);
+_putchar(' ');
+_putchar('0' + k);
 }
-else if (rep < 100)
+else if (k < 100)
 {
 _putchar(' ');
-_putchar('0' + rep / 10);
-_putchar('0' + rep % 10);
+_putchar(' ');
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
 }
 else
 {
-_putchar('o' + rep / 100);
-_putchar('0' + (rep - 100) / 10);
-_putchar('0' + rep % 10);
+_putchar(' ');
+_putchar((k / 100) + '0');
+_putchar(((k % 100) / 10) + '0');
+_putchar(((k % 100) % 100) + '0');
 }
-if (b < n)
-{
-_putchar(',');
+if (j < n)
 _putchar(' ');
 }
-}
 _putchar('\n');
-a++;
+}
 }
 }
