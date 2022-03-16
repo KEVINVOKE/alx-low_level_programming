19a1d234a45919d94e32a84ad384190a467d4f0e
KEVINVOKE <kevinopon730@gmail.com>
2022-03-16 02:26:32 -0700

    My commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,12 @@
-include "main.h"
+#include "main.h"
 /**
 *main - tests function that prints if integer is positive or negative.
-8Return: 0
+*Return: 0
 */
 int main(void)
 {
 int i;
 i = 0;
-positive_or-negative(i);
+positive_or_negative(i);
 return (0);
 }
