86da59cc64ab1754746b4fc9353739492c80f496
KEVINVOKE <kevinopon730@gmail.com>
2022-03-17 06:53:27 -0700

    My commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,13 +6,16 @@
 void print_most_numbers(void)
 {
 int i;
-for (i = 0; i < 10; i++)
+for (i = 0; i <= 9; i++)
 {
-if ((i == 2) || (i == 4))
+if (i == 2 || i == 4)
 {
 continue;
 }
-_putchar('o' + i);
+else
+{
+_putchar(i + '0');
+}
 }
 _putchar('\n');
 }
