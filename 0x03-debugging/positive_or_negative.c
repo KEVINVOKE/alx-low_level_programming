88147418767a8ecbaad9e714d0dddeac8aca3e3a
KEVINVOKE <kevinopon730@gmail.com>
2022-03-17 01:50:17 -0700

    My commit

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,18 @@
+/**
+*main - entry point
+*Return: always 0 (success)
+*/
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+void positive_or_negative(int n)
+{
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n == 0)
+printf("%d is zero\n", n);
+else
+printf("%d is negative\n", n);
+}
