63da52c59800be11fd797a078884d86feff485af
KEVINVOKE <kevinopon730@gmail.com>
2022-03-16 23:25:33 -0700

    My commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 void positive-or_negative(int i);
-int largest_number(int, int, int);
+int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining-days(int month, int day, int year);
 #endif /* MAIN_H */
