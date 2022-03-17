6830b0804a1e70ffafd25439120bd00ac5806d53
KEVINVOKE <kevinopon730@gmail.com>
2022-03-17 04:35:44 -0700

    My commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*_isupper - check for uppercase characters
+*@c: integer holding the character
+*return: 1- uppercase character 0- Not
+*/
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+return (1);
+return (0);
+}
