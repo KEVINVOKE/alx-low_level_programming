284a409df430b2565add335f469f38866db84eed
KEVINVOKE <99311384+KEVINVOKE@users.noreply.github.com>
2022-03-28 13:10:16 +0300

    Update 4-strpbrk.c
    My commit

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,46 +1,40 @@
 #include "main.h"
 #include <stdio.h>
-
 /**#include "main.h"
-
 /**
- * print_chessboard - prints chessboard.
- * @a: matrix
+* print_chessboard - prints chessboard.
+* @a: matrix
 */
-
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
-
-	for (i = 0; i < 8; i++)
-	{
-		for (j = 0; j < 8; j++)
-		{
-			_putchar(*(*(i + a) + j));
-		}
-	_putchar('\n');
-	}
+int i, j;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+_putchar(*(*(i + a) + j));
 }
- * _strpbrk - bytes
- * @s: pointer to char
- * @accept: pointer to char
- * Return: NULL
- */
-
+_putchar('\n');
+}
+}
+*_strpbrk - bytes
+*@s: pointer to char
+*@accept: pointer to char
+*Return: NULL
+*/
 char *_strpbrk(char *s, char *accept)
 {
-	int i;
-
-	while (*s)
-	{
-		for (i = 0; accept[i]; i++)
-		{
-			if (*s == accept[i])
-			{
-				return (s);
-			}
-		}
-		s++;
-	}
-	return (NULL);
+int i;
+while (*s)
+{
+for (i = 0; accept[i]; i++)
+{
+if (*s == accept[i])
+{
+return (s);
+}
+}
+s++;
+}
+return (NULL);
 }
