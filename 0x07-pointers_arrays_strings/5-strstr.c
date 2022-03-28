5180e8963eddc390d588507b50fe29ca039f187f
KEVINVOKE <99311384+KEVINVOKE@users.noreply.github.com>
2022-03-28 12:51:31 +0300

    Update 5-strstr.c
    My commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,12 +1,10 @@
 #include "main.h"
-#include <stdio.h>
 /**
 *_strstr-function locate
 *@haystack: pointer to char
 *@needle: pointer to char
 *Return: 0
 */
-char *_strstr(char *haystack, char *needle)
 char *_strstr(char *haystack, char *needle)
 {
 	char *result = haystack, *fneedle = needle;
