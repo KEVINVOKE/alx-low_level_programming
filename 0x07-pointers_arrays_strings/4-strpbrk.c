4f163b02da3df201737483c859fa9cd6722c32d3
KEVINVOKE <99311384+KEVINVOKE@users.noreply.github.com>
2022-03-28 13:02:50 +0300

    Update and rename 4-strpbk.c to 4-strpbrk.c
    My commit

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,26 @@
 #include "main.h"
 #include <stdio.h>
 
+/**#include "holberton.h"
+
 /**
+ * print_chessboard - prints chessboard.
+ * @a: matrix
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(*(*(i + a) + j));
+		}
+	_putchar('\n');
+	}
+}
  * _strpbrk - bytes
  * @s: pointer to char
  * @accept: pointer to char
