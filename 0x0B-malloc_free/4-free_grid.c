2bbd88004521d81e7089f187d91f6d1a647e74d3
KEVINVOKE <99311384+KEVINVOKE@users.noreply.github.com>
2022-04-05 20:03:21 +0300

    Update 4-free_grid.c
    My commit

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,42 +1,17 @@
-#include <stdlib.h>
-<<<<<<< HEAD
-<<<<<<< HEAD
+#include <stdio.h>
 #include "main.h"
-=======
 
->>>>>>> 8657fca78602de11eb524c9fb9bb9db3ad4320f1
-=======
-
->>>>>>> 8657fca78602de11eb524c9fb9bb9db3ad4320f1
 /**
- * free_grid - Free a 2 dimensional grid previously created by your
- * alloc_grid function
- *
- * @grid: The grid we'll free up
- * @height: The number of rows in the grid
- *
- * Return: Nothing
+ * free_grid - frees a 2 dimensional grid previously
+ * created by the alloc_grid function
+ * @grid: double pointer to multi-dimen array
+ * @height: height of grid
  */
-
 void free_grid(int **grid, int height)
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-int i;
-for (i = 0; i < height; i++)
-=======
-=======
->>>>>>> 8657fca78602de11eb524c9fb9bb9db3ad4320f1
-	int i = 0;
+	int i;
 
-	while (i < height)
-	{
-<<<<<<< HEAD
->>>>>>> 8657fca78602de11eb524c9fb9bb9db3ad4320f1
-=======
->>>>>>> 8657fca78602de11eb524c9fb9bb9db3ad4320f1
+	for (i = 0; i < height; i++)
 		free(grid[i]);
-		i++;
-	}
 	free(grid);
 }
