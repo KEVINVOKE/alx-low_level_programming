29e0d9f04bdc3e41f655568ef63590d1ed30942a
KEVINVOKE <99311384+KEVINVOKE@users.noreply.github.com>
2022-04-05 08:03:03 +0300

    Update 0-create_array.c
    My commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,20 +8,18 @@
  *
  * Return: a pointer to the array, or NULL if it fails
  */
-
 char *create_array(unsigned int size, char c)
 {
-	unsigned int i = 0;
-	char *arr;
-
-	arr = (char *) malloc(size);
-	if (size == 0 || arr == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < size; i++)
-	{
-		arr[i] = c;
-	}
-	return (arr);
+unsigned int i = 0;
+char *arr;
+arr = (char *) malloc(size);
+if (size == 0 || arr == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < size; i++)
+{
+arr[i] = c;
+}
+return (arr);
 }
