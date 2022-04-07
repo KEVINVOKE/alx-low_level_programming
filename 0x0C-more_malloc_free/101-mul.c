42e1462f4a08d237ae60fca879d58062ac3fb310
KEVINVOKE <99311384+KEVINVOKE@users.noreply.github.com>
2022-04-07 20:54:42 +0300

    Update 101-mul.c
    My commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,54 +1,96 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * _isdigit - checks digit
- *
- * @s: string to check
- *
- * Return: 1 if digit, 0 if not
- */
-int _isdigit(char *s)
-{
-int count = 0;
+* is_digit - checks if a string contains a non-digit char
+* @s: string to be evaluated
+* Return: 0 if a non-digit is found, 1 otherwise
+*/
 
-while (*(s + count))
+int is_digit(char *s)
 {
-if (*(s + count) > '9' || *(s + count) < '0')
+int i = 0;
+while (s[i])
+{
+if (s[i] < '0' || s[i] > '9')
 return (0);
-count++;
+i++;
 }
 return (1);
 }
 
 /**
- * main - prints multiple of to numbers
- *
- * @argc: number of argument
- * @argv: arguments passed
- *
- * Return: 0 on success
- */
+* _strlen - returns the length of a string
+* @s: string to evaluate
+* Return: the length of the string
+*/
+
+int _strlen(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+return (i);
+}
+
+/**
+* errors - handles errors for main
+*/
+
+void errors(void)
+{
+printf("Error\n");
+exit(98);
+}
+
+/**
+* main - multiplies two positive numbers
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: always 0 (Success)
+*/
+
 int main(int argc, char *argv[])
 {
-char num1, num2;
-unsigned int res;
-
-if (argc != 3)
+char *s1, *s2;
+int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+s1 = argv[1], s2 = argv[2];
+if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+errors();
+len1 = _strlen(s1);
+len2 = _strlen(s2);
+len = len1 + len2 + 1;
+result = malloc(sizeof(int) * len);
+if (!result)
+return (1);
+for (i = 0; i <= len1 + len2; i++)
+result[i] = 0;
+for (len1 = len1 - 1; len1 >= 0; len1--)
 {
-printf("Error\n");
-exit(98);
-}
-
-if (!_isdigit(argv[1]) || !_isdigit(argv[2]))
+digit1 = s1[len1] - '0';
+carry = 0;
+for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 {
-printf("Error\n");
-exit(98);
+digit2 = s2[len2] - '0';
+carry += result[len1 + len2 + 1] + (digit1 * digit2);
+result[len1 + len2 + 1] = carry % 10;
+carry /= 10;
 }
-
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-res = num1 *num2;
-printf("%d\n", res);
+if (carry > 0)
+result[len1 + len2 + 1] += carry;
+}
+for (i = 0; i < len - 1; i++)
+{
+if (result[i])
+a = 1;
+if (a)
+_putchar(result[i] + '0');
+}
+if (!a)
+_putchar('0');
+_putchar('\n');
+free(result);
 return (0);
 }
