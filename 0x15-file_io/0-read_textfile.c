c9a889f021154b2cab02d8fd46e4dc7cecb4164d
KEVINVOKE <99311384+KEVINVOKE@users.noreply.github.com>
2022-05-03 16:36:59 +0300

    Update 0-read_textfile.c
    My commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,45 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * read_textfile - a function that reads a
- * text file and prints it to the POSIX standard output
- * @filename: of the file
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: pointer to the file name
  * @letters: the number of letters it should read and print
- * Return: the actual number of letters it could read and print
- */
+ * Return:  the actual number of letters it could read and print
+*/
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	register int _open, _read, _write;
-	register char *buffer = malloc(sizeof(char)  * letters);
 
-	if (!(buffer))
-	{
-		free(buffer);
-		return (0);
-	}
-	if (!(filename))
-	{
-		return (0);
-	}
-	_open = open(filename, O_RDONLY);
-	_read = read(_open, buffer, letters);
-	_write = write(STDOUT_FILENO, buffer, _read);
+int file;
+ssize_t rcount, wcount;
+char *buffer;
 
-	if (_open == -1 || _read == -1 || _write == -1 || !(_write == _read))
-	{
-		free(buffer);
-		return (0);
-	}
+if (filename == NULL)
+	return (0);
+
+file = open(filename, O_RDWR);
+if (file == -1)
+	return (0);
+
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+{
 	free(buffer);
-	close(_open);
-	return (_write);
+	return (0);
+}
+
+rcount = read(file, buffer, letters);
+if (rcount == -1)
+	return (0);
+
+wcount = write(STDOUT_FILENO, buffer, rcount);
+
+if (wcount == -1 || rcount != wcount)
+	return (0);
+
+free(buffer);
+
+close(file);
+
+return (wcount);
 }
