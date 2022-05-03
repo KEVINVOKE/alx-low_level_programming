295d6d5d2bba0e2d27b13181d095352a98cfec95
KEVINVOKE <99311384+KEVINVOKE@users.noreply.github.com>
2022-05-03 17:14:04 +0300

    Update 0-read_textfile.c
    My commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,41 +1,37 @@
 #include "main.h"
-
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
- * @filename: A pointer to the name of the file.
- * @letters: The number of letters the
- *           function should read and print.
- *
- * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print.
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ * Return: the number of letters printed, or 0 if it failed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;
-int s, t;
-char *buf;
-if (!filename)
-return (0);
-fd = open(filename, O_RDONLY);
-if (fd < 0)
-return (0);
-buf = malloc(sizeof(char) * letters);
-if (!buf)
-return (0);
-s = read(fd, buf, letters);
-if (s < 0)
-{
-free (buf);
-return (0);
-}
-buf[s] = '\0';
-close(fd);
-t = write(STDOUT_FILENO, buf, s);
-if (t < 0)
-{
-free(buf);
-return (0);
-}
-free(buf);
-return (t);
+	int fd;
+	int i, y;
+	char *buf;
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+	i = read(fd, buf, letters);
+	if (i < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	buf[i] = '\0';
+	close(fd);
+	y = write(STDOUT_FILENO, buf, i);
+	if (y < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	return (y);
 }
