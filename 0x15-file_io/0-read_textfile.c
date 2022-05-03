77359ff2ebdd5927954361d5c0358af3872b5efd
KEVINVOKE <99311384+KEVINVOKE@users.noreply.github.com>
2022-05-03 17:05:40 +0300

    Update 0-read_textfile.c
    My commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,28 +12,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
-	char *buffer;
-
-	if (filename == NULL)
-		return (0);
-
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
-
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
-	{
-		free(buffer);
-		return (0);
-	}
-
-	free(buffer);
-	close(o);
-
-	return (w);
+int fd;
+int s, t;
+char *buf;
+if (!filename)
+return (0);
+fd = open(filename, O_RDONLY);
+if (fd < 0)
+return (0)
+buf= malloc(sizeof(char) * letters);
+if (!buf)
+return (0);
+s = read(fd, buf, letters);
+if (s < 0)
+{
+free (buf);
+return (0);
+}
+buf[s] = '\0';
+close(fd);
+t = write(STDOUT_FILENO, buf, s);
+if (t < 0)
+{
+free(buf);
+return (t);
 }
