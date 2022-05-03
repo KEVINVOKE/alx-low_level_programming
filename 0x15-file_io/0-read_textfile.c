964246333b75ece49ecffa83c16d51eb5955750c
KEVINVOKE <99311384+KEVINVOKE@users.noreply.github.com>
2022-05-03 17:09:22 +0300

    Update 0-read_textfile.c
    My commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,8 +18,8 @@ if (!filename)
 return (0);
 fd = open(filename, O_RDONLY);
 if (fd < 0)
-return (0)
-buf= malloc(sizeof(char) * letters);
+return (0);
+buf = malloc(sizeof(char) * letters);
 if (!buf)
 return (0);
 s = read(fd, buf, letters);
@@ -32,7 +32,7 @@ buf[s] = '\0';
 close(fd);
 t = write(STDOUT_FILENO, buf, s);
 if (t < 0)
-{
+}
 free(buf);
 return (t);
 }
